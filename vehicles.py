700d82421f7251bec6ae2c63fbd8320f688e5a8b
saanvi.code <sa.manni.g9@gmail.com>
2025-05-17 18:51:15 +0530

    gjm

---
+++ vehicles.py
@@ -0,0 +1,9 @@
+class Vehicles:
+    def __init__(self,maxspeed,mileage):
+        self.m=maxspeed
+        self.mile=mileage
+
+tata=Vehicles(140,54)
+
+print("the max speed is",tata.m)
+print("the mileage is",tata.mile)
